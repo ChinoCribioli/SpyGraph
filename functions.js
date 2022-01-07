3a305d65060ad2375d85faf2099733dce4fc5002
ChinoCribioli <ezequiel.cribioli@gmail.com>
2022-01-07 13:09:47 -0300

    testing query.php

--- functions.js
+++ functions.js
@@ -48,6 +48,7 @@ function new_song(link){
 
 function query(artist1, artist2){
   var id1 = /track\/([^/^\s\?]{22})/g.exec(artist1), id2 = /track\/([^/^\s\?]{22})/g.exec(artist2);
+  console.log(id1);
   if(id1 == null || id2 == null){
     //TODO: error message
     return;
@@ -59,7 +60,7 @@ function query(artist1, artist2){
     id1 = id2;
     id2 = aux;
   }
-  console.log("llegue");
+  console.log(id1,id2);
   fetch("query.php", {
     method: "POST",
     headers: {
