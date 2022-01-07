e213673d287d305a61886a7c6aa3be2f2330faee
ChinoCribioli <ezequiel.cribioli@gmail.com>
2022-01-07 16:09:36 -0300

    added Ds extension using composer.json

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'cbd7f6697f05484de6be06a72da7f8aa917e514b',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'cbd7f6697f05484de6be06a72da7f8aa917e514b',
+    ),
+    'php-ds/php-ds' => 
+    array (
+      'pretty_version' => 'v1.4.0',
+      'version' => '1.4.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '298cafa4e0e20aeba4d63644e3de694e7cf83ffd',
+    ),
+  ),
+);
