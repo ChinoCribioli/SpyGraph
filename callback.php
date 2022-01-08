13d4ba5adbedfbba47174cd03daa9356910c9927
ChinoCribioli <ezequiel.cribioli@gmail.com>
2022-01-08 19:58:26 -0300

    managin API from server

--- callback.php
+++ callback.php
@@ -4,7 +4,8 @@ require 'vendor/autoload.php';
 $session = new SpotifyWebAPI\Session(
     'ab57d209083846459efd255b1b62b4d5',
     '237e31d7c23e4265af9d5367ff02ce86',
-    'https://developer.spotify.com/dashboard/applications/ab57d209083846459efd255b1b62b4d5'
+    //'https://developer.spotify.com/dashboard/applications/ab57d209083846459efd255b1b62b4d5'
+    'https://spoti-graph.herokuapp.com/test.html'
 );
 
 $state = $_GET['state'];
