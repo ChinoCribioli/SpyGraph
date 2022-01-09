9398a3638b3572c6cb120ea9879ac73a366d633f
ChinoCribioli <ezequiel.cribioli@gmail.com>
2022-01-08 21:19:46 -0300

    managing API from server

--- auth.php
+++ auth.php
@@ -4,19 +4,12 @@ require 'vendor/autoload.php';
 $session = new SpotifyWebAPI\Session(
     'ab57d209083846459efd255b1b62b4d5',
     '237e31d7c23e4265af9d5367ff02ce86',
-    //'https://developer.spotify.com/dashboard/applications/ab57d209083846459efd255b1b62b4d5'
-    'https://spoti-graph.herokuapp.com/test.html'
 );
 
-$state = $session->generateState();
-$options = [
-    'scope' => [
-        'playlist-read-private',
-        'user-read-private',
-    ],
-    'state' => $state,
-];
+$session->requestCredentialsToken();
+$accessToken = $session->getAccessToken();
 
-header('Location: ' . $session->getAuthorizeUrl($options));
+
+header('Location: add-edges.php');
 die();
 ?>
