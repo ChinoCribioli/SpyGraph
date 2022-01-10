f4e2e1c32bc44b6754bbe220e7668520fe7b03eb
ChinoCribioli <ezequiel.cribioli@gmail.com>
2022-01-10 18:47:44 -0300

    adding playlists

--- add-playlist.php
+++ add-playlist.php
@@ -5,13 +5,12 @@ include("auth.php"); //set the access token
 include("upload_song.php");
 $playlistId = $_POST['playlistId'];
 $isPlaylist = $_POST['isPlaylist'];
-echo $isPlaylist . "\n";
 
-if($isPlaylist){
-    echo "entre: $isPlaylist \n";
+if($isPlaylist == "false"){//it's an string so I have to compare it with "false"
     $songs = $api->getPlaylist($playlistId)->tracks;
 }
 else{
+    echo "entre: $isPlaylist \n";
     $songs = $api->getAlbum($playlistId)->tracks;
 }
 foreach($songs as $track){
