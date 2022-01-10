393cbc3483264f7ab6cfeca1c9d7896ec307afaa
ChinoCribioli <ezequiel.cribioli@gmail.com>
2022-01-10 18:36:07 -0300

    adding playlists

--- add-playlist.php
+++ add-playlist.php
@@ -5,9 +5,15 @@ include("auth.php"); //set the access token
 include("upload_song.php");
 $playlistId = $_POST['playlistId'];
 $isPlaylist = $_POST['isPlaylist'];
+echo $isPlaylist . "\n";
 
-if($isPlaylist) $songs = $api->getPlaylist($playlistId)->tracks;
-else $songs = $api->getAlbum($playlistId)->tracks;
+if($isPlaylist){
+    $songs = $api->getPlaylist($playlistId)->tracks;
+    echo "entre \n";
+}
+else{
+    $songs = $api->getAlbum($playlistId)->tracks;
+}
 foreach($songs as $track){
     upload_song($track->id,$api);
 }
