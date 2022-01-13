15a2a7069666d8f4e163cbda0a4b318654dcfba1
ChinoCribioli <ezequiel.cribioli@gmail.com>
2022-01-13 19:37:00 -0300

    reading dbconfig from evirnoment vairable

--- server_config.php
+++ server_config.php
@@ -1,6 +1,14 @@
 <?php
-$servername = "us-cdbr-east-04.cleardb.com";
-$username = "bd2be9a1853f00";
-$password = "608acbeb";
-$dbname = "heroku_a7020db6550b501";
+// $servername = "us-cdbr-east-04.cleardb.com";
+// $username = "bd2be9a1853f00";
+// $password = "608acbeb";
+// $dbname = "heroku_a7020db6550b501";
+
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$servername = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$dbname = substr($url["path"], 1);
+
 ?>
